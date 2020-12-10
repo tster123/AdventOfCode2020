641db3d4d774dbd85ce4e8fb6ea13d053d0ab0fe
Tyler Boone <tboone@microsoft.com>
2020-12-10 15:11:00 -0800

    day 5

---
+++ Advent/Advent2020Tests/Days/D5/Day5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020Tests.Days.D5
+{
+    [TestClass]
+    public class Day5
+    {
+        [TestMethod]
+        public void Problem1()
+        {
+            Console.WriteLine(ToId("BBFBFBFLLL"));
+        }
+
+        [TestMethod]
+        public void Problem2()
+        {
+            string[] lines = File.ReadAllLines("./Days/D5/Data.txt");
+            var ints = lines.Select(s => ToId(s)).OrderBy(i => i).ToList();
+            for (int i = 1; i < ints.Count - 2; i++)
+            {
+                if (ints[i] + 2 == ints[i+1] && ints[i - 1] == ints[i] - 1)
+                {
+                    Console.WriteLine(ints[i] + 1);
+                }
+            }
+        }
+
+        public int ToId(string str)
+        {
+            return Convert.ToInt32(str.Substring(0, 7).Replace("F", "0").Replace("B", "1"), 2) * 8 +
+                Convert.ToInt32(str.Substring(7).Replace("L", "0").Replace("R", "1"), 2);
+        }
+    }
+}
---
+++ Advent/Advent2020Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Advent2020Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Advent2020Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a067c36f-ffa8-4984-a6fb-79827b9985a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
