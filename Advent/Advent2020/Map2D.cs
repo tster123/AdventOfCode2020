ca67dcdb8b6be9f809452e2756ff494ed6223e1a
Tyler Boone <tboone@microsoft.com>
2020-12-10 23:55:47 -0800

    adding map2d

--- Advent/Advent2020/Map2D.cs
+++ Advent/Advent2020/Map2D.cs
@@ -13,9 +13,11 @@ namespace Advent2020
 
         public T Get(int x, int y)
         {
-            if (x < 0 || y < 0 || x >= matrix.Length || y >= matrix[x].Length) return defaultValue;
+            if (!IsInside(x, y)) return defaultValue;
             return matrix[x][y];
         }
+
+        public bool IsInside(int x, int y) => x >= 0 && y >= 0 && x < matrix.Length && y < matrix[x].Length;
 
         public Point2D<T> this[int x, int y] => new Point2D<T>(this, x, y);
 
@@ -23,6 +25,35 @@ namespace Advent2020
         {
             this.matrix = matrix;
             this.defaultValue = defaultValue;
+        }
+
+        public Map2D(IEnumerable<IEnumerable<T>> matrix, T defaultValue = default)
+        {
+            this.matrix = matrix.Select(r => r.ToArray()).ToArray();
+            this.defaultValue = defaultValue;
+        }
+
+        public IEnumerable<Point2D<T>> GetAllPoints()
+        {
+            for (int x = 0; x < matrix.Length; x++)
+            for (int y = 0; y < matrix[x].Length; y++)
+                yield return this[x, y];
+        }
+
+        public static readonly Direction[] CardinalDirections =
+            new[] {Direction.Up, Direction.Left, Direction.Down, Direction.Right};
+
+        public static readonly Direction[] DiagonalDirections =
+            new[] { Direction.LeftUp, Direction.LeftDown, Direction.RightUp, Direction.RightDown };
+
+        public static readonly Direction[] AllDirections = CardinalDirections.Concat(DiagonalDirections).ToArray();
+    }
+
+    public static class MapFactories
+    {
+        public static Map2D<char> Character(string[] lines, char defaultValue = '~')
+        {
+            return new Map2D<char>(lines, defaultValue);
         }
     }
 
@@ -39,5 +70,24 @@ namespace Advent2020
         }
 
         public T Value => map.Get(X, Y);
+        public bool IsInside => map.IsInside(X, Y);
+
+        public Point2D<T> GetInDirection(Direction direction)
+        {
+            int x = X;
+            int y = Y;
+            if (direction.ToString().Contains("Left")) x--;
+            if (direction.ToString().Contains("Right")) x++;
+            if (direction.ToString().Contains("Up")) y++;
+            if (direction.ToString().Contains("Down")) y--;
+            return map[x, y];
+        }
+    }
+
+    public enum Direction
+    {
+        LeftUp, Up, RightUp,
+        Left, Right,
+        LeftDown, Down, RightDown
     }
 }
