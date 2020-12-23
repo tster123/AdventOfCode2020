b57bbaf727b0741d7479b8b5f42c7f27c7ce1fbb
Tyler Boone <tboone@microsoft.com>
2020-12-23 12:19:47 -0800

    day 22 p2

--- Advent/Advent2020Tests/Y2020/D22/Day22.cs
+++ Advent/Advent2020Tests/Y2020/D22/Day22.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using Advent2020Tests.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -59,7 +62,12 @@ namespace Advent2020Tests.Y2020.D22
                 give.Add(loss);
             }
 
-            var winner = game.Player1.Count > 0 ? game.Player1 : game.Player2;
+            return Score(game.Player1.Count > 0 ? game.Player1 : game.Player2);
+        }
+
+        private static object Score(List<int> winner)
+        {
+            
             long score = 0;
             for (int i = 0; i < winner.Count; i++)
             {
@@ -77,18 +85,111 @@ namespace Advent2020Tests.Y2020.D22
 
         private object Problem2(Game game)
         {
-            return null;
+            return Score(PlayGame(game).Winner);
+        }
+
+        private int hitMemoized = 0;
+        private readonly Dictionary<int, Dictionary<string, bool>> memoryGlobal = new Dictionary<int, Dictionary<string, bool>>();
+        private GameResult PlayGame(Game game)
+        {
+            for (int i = 0; i <= game.GameSize; i++)
+            {
+                memoryGlobal[i] = new Dictionary<string, bool>();
+            }
+
+            bool p1Win = PlayGameInternal(game);
+            return new GameResult(game, p1Win);
+        }
+
+        private bool PlayGameInternal(Game game)
+        {
+            HashSet<string> seen = new HashSet<string>();
+            var memory = memoryGlobal[game.GameSize];
+            string toMem = game.ToString();
+            while (game.Player1.Count > 0 && game.Player2.Count > 0)
+            {
+                string str = game.ToString();
+                if (seen.Contains(str))
+                {
+                    memory[toMem] = true;
+                    return memory[toMem];
+                }
+                seen.Add(str);
+                if (memory.TryGetValue(str, out var ret))
+                {
+                    hitMemoized++;
+                    return ret;
+                }
+                int p1 = game.Player1[0];
+                int p2 = game.Player2[0];
+                game.Player1.RemoveAt(0);
+                game.Player2.RemoveAt(0);
+
+                bool p1Win;
+                if (game.Player1.Count >= p1 && game.Player2.Count >= p2)
+                {
+                    p1Win = PlayGameInternal(new Game(game.Player1.Take(p1).ToList(), game.Player2.Take(p2).ToList()));
+                }
+                else
+                {
+                    p1Win = p1 > p2;
+                }
+                List<int> give = p1Win ? game.Player1 : game.Player2;
+                int win = p1Win ? p1 : p2;
+                int loss = p1Win ? p2 : p1;
+                give.Add(win);
+                give.Add(loss);
+            }
+            /*foreach (string s in seen)
+            {
+                memory[s] = result;
+            }*/
+            memory[toMem] = game.Player1.Count > game.Player2.Count;
+
+            return game.Player1.Count > game.Player2.Count;
         }
     }
 
+    public class GameResult
+    {
+        public Game Game;
+        public bool Player1Win;
+
+        public GameResult(Game game, bool player1Win)
+        {
+            Game = game;
+            Player1Win = player1Win;
+        }
+        public List<int> Winner => Player1Win ? Game.Player1 : Game.Player2;
+    }
+
     public class Game
-    {        
+    {
         public List<int> Player1, Player2;
 
         public Game(List<int> player1, List<int> player2)
         {
             Player1 = player1;
             Player2 = player2;
+        }
+
+        public int GameSize => Player1.Count + Player2.Count;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(Player1.Count + 1 + Player2.Count);
+            foreach (int i in Player1)
+            {
+                sb.Append((char) i);
+            }
+
+            sb.Append('|');
+            foreach (int i in Player2)
+            {
+                sb.Append((char)i);
+            }
+
+            return sb.ToString();
         }
     }
 }
